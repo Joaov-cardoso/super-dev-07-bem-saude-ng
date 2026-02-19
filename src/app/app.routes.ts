17b39edf83c033fdc19f174ea3aa521a0439accb
Joaov-cardoso <joaovcardosom@gmail.com>
2026-02-18 22:01:43 -0300

    Aula 18-02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,22 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guards';
 
 export const routes: Routes = [
-    {path: 'recepcionistas', loadComponent: () => import('./pages/recepcionistas/list/list').then((m) => m.List)},
-    {path: 'pacientes', loadComponent: () => import('./pages/pacientes/list/list').then((m) => m.List)},
-    {path: 'consultas', loadComponent: () => import('./pages/consultas/list/list').then((m) => m.List)},
-    {path: 'financeiro', loadComponent: () => import('./pages/financeiro/list/list').then((m) => m.List)},
+    {
+        path: 'callback',
+        loadComponent: () => import('./pages/callback/callback').then((m) => m.Callback)
+
+    },
+    {
+        path: '',
+        loadComponent: () => import('./layout/layout').then((m) => m.Layout),
+        canActivate: [authGuard],
+        children: [
+            { path: 'recepcionistas', loadComponent: () => import('./pages/recepcionistas/list/list').then((m) => m.List) },
+            { path: 'pacientes', loadComponent: () => import('./pages/pacientes/list/list').then((m) => m.List) },
+            { path: 'consultas', loadComponent: () => import('./pages/consultas/list/list').then((m) => m.List) },
+            { path: 'financeiro', loadComponent: () => import('./pages/financeiro/list/list').then((m) => m.List) },
+        ]
+
+    }
 ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,10 @@
 export const environment = {
     producion: false,
-    apiUrl: "http://localhost:8000"
+    apiUrl: "http://localhost:8000",
+    auth0 :{
+        domain: "dev-ob77nzgp507qcp45.us.auth0.com",
+        clientId: "lh8VMNjlJ3JEat9o6ob8EL7xBqoVdBoR",
+        audience: "019c72de-d842-7872-9764-598f75faa0c2",
+        redirectUri: "http://localhost:4200/callback"
+    }
 }
